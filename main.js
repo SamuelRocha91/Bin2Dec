2570882f3e7e228542fb94223297853df2f80450
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-11-02 22:27:04 -0300

    adiciona lógica de conversão e inserção na tela

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // captura o button e o input no Dom
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+const p = document.querySelector('p');
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log(input.value)
+  const resultado = conversor(input.value);
+  p.innerText = `${input.value} em decimal equivale a ${resultado}`
+  input.value = '';
 })
 
 // impede e notifica o usuário se algo diferente de zero ou um for inserido
@@ -15,3 +18,14 @@ input.addEventListener('input', () => {
     window.alert('Somente é permitido inserir zero ou um')
   }
 })
+
+// cria a lógica de conversão do binário para decimal
+const conversor = (string) => {
+  let valor = 0;
+  let potencia = string.length - 1;
+  for (let i = 0; i < string.length; i += 1) {
+    valor += (Number(string[i]) * (2 ** potencia));
+    potencia -= 1
+  }
+  return valor
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,25 +1,11 @@
-function smallestCommons(arr) {
-  let maior = arr[0] > arr[1] ? arr[0] : arr[1];
-  let menor = arr[0] > arr[1] ? arr[1] : arr[0];
-  let bool = true;
-  let j = maior + 55;
-  while (bool) {
-    if(verifyDivisors(menor, maior, j) == 1) {
-      bool = false;
-    }
-    j += 1
+const conversor = (string) => {
+  let valor = 0;
+  let potencia = string.length - 1;
+  for (let i = 0; i < string.length; i += 1) {
+    valor += (Number(string[i]) * (2 ** potencia));
+    potencia -= 1
   }
-  return j;
+  return valor
 }
 
-
-function verifyDivisors(inicio, fim, num) {
-  for(let i = inicio; inicio <= fim; i += 1) {
-    if (num % i !== 0) {
-      return 0
-    }
-  }
-  return 1
-}
-
-console.log(smallestCommons([1,5]));
+console.log(conversor('101'))
