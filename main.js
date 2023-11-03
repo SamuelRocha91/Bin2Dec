fb2bc6f21b467cc74b0d9ed5f57b0a4aeef51ef8
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-11-02 22:14:47 -0300

    cria listner pra impedir o usuário de inserir informação diferente de 0 ou 1

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const input = document.querySelector('input'); 
+// captura o button e o input no Dom
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(input.value)
+})
+
+// impede e notifica o usuário se algo diferente de zero ou um for inserido
+input.addEventListener('input', () => {
+  let value = input.value;
+  if(!(/[0-1]/.test(Number(value[value.length - 1])))) {
+    input.value = value.slice(0, value.length - 1)
+    window.alert('Somente é permitido inserir zero ou um')
+  }
+})
